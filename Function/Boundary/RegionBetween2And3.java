91c6a5cadb1f5b6b45ca922969c63aaf1611bc0c
Yakov Alekseev <40690132+politexnik@users.noreply.github.com>
2018-10-01 11:18:47 +0300

    Зоны 1, 2 и 3
    Разработаны функции зон 1, 2, и 3

---
+++ Function/Boundary/RegionBetween2And3.java
@@ -0,0 +1,36 @@
+package Function.Boundary;
+
+public class RegionBetween2And3 {
+    private static double[] boundaryRegions2And3Coeffiсiens = {  0,
+            0.34805185628969e+3,
+            -0.11671859879975e+1,
+            0.10192970039326e-2,
+            0.57254459862746e+3,
+            0.13918839778870e+2 };
+
+    public static double boundaryTemperatureBetweenRegions2And3(double pressure) {
+        if (pressure >= 16.5291643e6 && pressure <= 100e6) {
+            double[] n = boundaryRegions2And3Coeffiсiens;
+            double t_ = 1;   // t* = 1K
+            double p_ = 1e+6;   //p* = 1МПа
+            double pi = pressure/p_ ;   //относительное давление
+
+            double tetha = n[4] + Math.pow((pi - n[5]) / n[3], 0.5);  //      T/T*
+            return tetha*t_;
+        }
+        return 0;
+    }
+
+    public static double boundaryPressureBetweenRegions2And3(double temperature) {
+        if (temperature>=623.15 && temperature<=863.15) {
+            double[] n = boundaryRegions2And3Coeffiсiens;
+            double t_ = 1;   // t* = 1K
+            double p_ = 1e+6;   //p* = 1МПа
+            double tetha = temperature/t_;
+
+            double pi = n[1] + n[2]* tetha+ n[3]* Math.pow(tetha,2);    //относительное давление
+            return pi*p_;
+        }
+        return 0;
+    }
+}
---
+++ Function/Test.java
@@ -0,0 +1,27 @@
+package Function;
+
+import Function.Boundary.*;
+import Function.Region1.BasicEquation;
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        //PropertyRegion e = PropertyRegion.find(123, 123);
+
+        //System.out.println(RegionBetween2And3.boundaryPressureBetweenRegions2And3(623.15));
+        //System.out.println(RegionBetween2And3.boundaryTemperatureBetweenRegions2And3(0.165291643e+8));
+//        System.out.println(SaturationLine.pressure(300));
+//        System.out.println(SaturationLine.pressure(500));
+//        System.out.println(SaturationLine.pressure(600));
+//        System.out.println(SaturationLine.temperature(0.1e+6));
+//        System.out.println(SaturationLine.temperature(1.0e+6));
+//        System.out.println(SaturationLine.temperature(10.0e+6));
+//        System.out.println(PropertyRegion.find(50e+6,623));
+
+        System.out.println(BasicEquation.isobaricHeatCapacity(3e+6, 300));
+        System.out.println(BasicEquation.isobaricHeatCapacity(80e+6, 300));
+        System.out.println(BasicEquation.isobaricHeatCapacity(3000000, 500));
+    }
+
+}
